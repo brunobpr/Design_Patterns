59db2bd6d4a673b996ded95ba563b0ee2a5922ae
Bruno <brunopr.bpr@gmail.com>
2020-03-25 18:44:34 +0000

    CLI Menu Created

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,16 +1,79 @@
 package controller;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
-import model.*;;
+import model.*;
+import view.*;
 
 public class Controller {
-		
-	public Controller() {
-		Database database = Database.getInstance();
-		
-		MySQLCountry c = new MySQLCountry() ;
 	
-		System.out.println(c.findCountryByCode("USA"));
+	private MenuView menu;
+	private BufferedReader reader;
+	private MySQLCountry sqlCountry;
+	private Database database = Database.getInstance();
+	
+	public Controller() {
+		/*Database database = Database.getInstance();
+		Continent cont =  Continent.AFRICA;
+		Country c = new Country.CountryBuilder("ARL", "Andromeda", cont, 349851).setHeadOfState("Dilmae").build();
+		MySQLCountry sql = new MySQLCountry() ;
+	
+		System.out.println(sql.addNewCountry(c));
+		System.out.println(sql.findCountryByCode("ARL"));*/
+		sqlCountry = new MySQLCountry();
+		menu = new MenuView();
+		reader = new BufferedReader(new InputStreamReader(System.in));
+		getMenuOption();
+		
+	}
+
+	private void getMenuOption() {
+		menu.options();
+		String optionSelected = getInput();;
+		switch(optionSelected) {
+			case "1":
+				menu.showAllContries(sqlCountry.getCountries());
+				getMenuOption();
+			case "2":
+				countryByName();
+			case "3":
+				countryByCode();
+			case "4":
+				
+			case "0":
+				System.exit(0);;
+		};
+		
+	}
+
+	private String getInput() {
+		String input;
+		try {
+			input = reader.readLine();	
+			return input;
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.print("Try again: ");
+			return getInput();
+		}
+		
+	}
+
+	private void countryByName() {
+		System.out.println("Find a country by Name");
+		System.out.print("Name: ");
+		String name = getInput();
+		menu.showCountry(sqlCountry.findCountryByName(name), name);
+		getMenuOption();
 	}
 	
+	private void countryByCode() {
+		System.out.println("Find a country by code");
+		System.out.print("Code: ");
+		String code = getInput();
+		menu.showCountry(sqlCountry.findCountryByCode(code), code);
+		getMenuOption();
+	}
 	
 }
---
+++ src/view/MenuView.java
@@ -0,0 +1,51 @@
+package view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import model.Country;
+
+public class MenuView {
+
+	public MenuView() {
+		System.out.println("Bruno Pereira Ribeiro - 2017138\nCCT College Dublin");
+		System.out.println("OODB - Pattern Combination");
+		divider();
+		System.out.println("\nWELCOME TO WORLD DB\n");
+	}
+
+	public void options() {
+		System.out.println("Please, choose one option:");
+		System.out.println("| 1 | - List all countries");
+		System.out.println("| 2 | - Find a country by country code");
+		System.out.println("| 3 | - Find a country by name");		
+		System.out.println("| 5 | - Exit");	
+	}
+
+	private void divider() {
+		System.out.println("________________________________________");
+	}
+
+	private void clean() {
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	}
+
+	public void showAllContries(ArrayList<Country> countries) {
+		clean();
+		System.out.println("List of All Countries");
+		for(Country country : countries) {
+			System.out.println(country);
+		}
+	}
+
+	public void showCountry(Country country, String countrySearched) {
+		clean();
+		if(country != null) {
+			System.out.println("Country found: " + country.getName());
+			divider();
+			System.out.println(country);
+		}
+		else System.out.println(countrySearched + " does not exist!\n");
+	}
+}
